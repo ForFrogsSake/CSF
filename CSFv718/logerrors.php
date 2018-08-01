4e8271cc6a95acbc00ac2ea733448840d6263f80
carylmariee <2161397@slu.edu.ph>
2018-08-01 13:13:56 +0800

    update

--- CSFv718/logerrors.php
+++
@@ -1,7 +0,0 @@
-<?php if(count($logerrors) > 0): ?>
-    <div class="error">
-        <?php foreach ($logerrors as $lerror) : ?>
-          <p><?php echo $lerror; ?></p>
-        <?php endforeach ?>
-    </div>
-<?php  endif ?>
--- profile.php
+++ profile.php
@@ -1,7 +1,7 @@
 <?php 
 
     $host = 'localhost';
-	$user = 'root';
+	$user = 'root1';
 	$pass = '';
 	$dbname = 'csf';
     
@@ -64,20 +64,28 @@
                     while($row = mysqli_fetch_assoc($result)){
                         echo "<style>";
                         echo "form {text-align: center;}";
-                        echo "label {float:left; margin:10px; margin-left:15%;}";
-                        echo "input[type=text], input[type=password] { width:50%; padding: 10px; margin:10px; }";
+                        echo ".label {float:left; margin:15px; margin-left:15%; margin-right:45px; }";
+                        echo "input[type=text], input[type=password] { width:40%; padding: 15px; margin:15px; }";
+                        echo "#confirm{ float:left; margin-left:15%; margin-top:15px; }";
+                        echo "input[name=confirmpass]{ width:40%; padding: 15px; margin-right:15px;  margin-top:15px;  margin-bottom:15px; margin-left:0;}";
+                        echo "#savebtn{  background-color: #0080ff; color: white; padding: 14px 20px; margin: 8px 0; border: none; cursor: pointer; width: 20%;}";
+                        echo "#cancelbtn{  background-color: #FF6347; color: white; padding: 14px 20px; margin: 8px 0; border: none; cursor: pointer; width: 20%;}";
                         echo "</style>";
                         echo "<form id='managetable'>";
-                        echo "<label>First Name:</label>";
-                        echo "<input type=text value='".$row['fname']."' readonly><br>";
-                        echo "<label>Last Name:</label>";
-                        echo "<input type=text value='".$row['lname']."' readonly><br>";
-                        echo "<label>Email:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</label>";
+                        echo "<label class='label'>First Name:</label>";
+                        echo "<input type=text value='".$row['fname']."' ><br>";
+                        echo "<label class='label'>Last Name:</label>";
+                        echo "<input type=text value='".$row['lname']."' ><br>";
+                        echo "<label class='label'>Email:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</label>";
                         echo "<input type=text value='".$row['email']."' readonly><br>";
-                        echo "<label>Username:</label>";
+                        echo "<label class='label'>Username:</label>";
                         echo "<input type=text value='".$row['username']."' readonly><br>";
-                        echo "<label>Password:</label>";
-                        echo "<input type=password value='".$row['password']."' readonly>";
+                        echo "<label class='label'>Password:</label>";
+                        echo "<input type=password ><br>";
+                        echo "<label id='confirm'>Confirm Password:</label>";
+                        echo "<input type=password name='confirmpass' ><br>";
+                        echo "<button type='submit' id='savebtn' name='save'>Save</button>&nbsp;";
+                        echo "<button type='submit'  id='cancelbtn' name='cancel'>Cancel</button>";
 
                         }
                             $result->free();
